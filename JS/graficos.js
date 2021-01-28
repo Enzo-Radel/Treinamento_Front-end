1d641b185a66498c561010fe072664e4e18f47e1
Enzo-Radel <enzo@radel.com.br>
2021-01-27 23:56:49 -0300

    troca de elementos entre as caixas (bug na mudanca de posicao dentro da caixa)

--- JS/graficos.js
+++ JS/graficos.js
@@ -3,17 +3,17 @@ var volumeDeChamadas = new Chart(document.getElementById('volumeDeChamadas').get
     data: {
         datasets: [
         {
-            label: 'Indice Pluviometrico',
-            data: [184, 155, 125, 81, 92, 98, 78, 76, 116, 133, 109, 143],
-            backgroundColor: "rgba(233,240,253,1)",
-            borderColor: "rgba(52,116,224,1)",
-            borderWidth: 3
-        },{
             type: 'line',
             label: 'Temperatura',
-            data: [21,20.4,18.2,16.2,13.4,13.5,14.2,15.3,16.7,18.4,19.8,18.4],
-            backgroundColor: "rgba(253,245,218,1)",
+            data: [21,20,18,16,13,13,14,15,16,18,19,18],
+            backgroundColor: "rgba(253,245,218,0.5)",
             borderColor: "rgba(248,204,70,1)",
+            borderWidth: 3
+        },{
+            label: 'Indice Pluviometrico',
+            data: [42, 40, 36, 32, 26, 26, 28, 30, 32, 36, 38, 36],
+            backgroundColor: "rgba(233,240,253,0.5)",
+            borderColor: "rgba(52,116,224,1)",
             borderWidth: 3
         }],
         labels: ["Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"]
@@ -25,7 +25,90 @@ var volumeDeChamadas = new Chart(document.getElementById('volumeDeChamadas').get
     }
 });
 
+// --------------------------------------------------------------------------
 
+const direita = document.getElementById("direita")
+const esquerda = document.getElementById("esquerda")
+const cima = document.getElementById("cima")
+const baixo = document.getElementById("baixo")
+const caixaEsquerda = document.getElementById("caixa-esquerda")
+const caixaDireita = document.getElementById("caixa-direita")
+const botoes = document.getElementById("botoes")
+
+botoes.addEventListener("click",(e) => {
+    const btnClicado = e.target.getAttribute("id")
+    const listaEsquerda = document.getElementById("select-from")
+    const listaDireita = document.getElementById("select-to")
+    let numOpcoes
+
+    switch (btnClicado) {
+        case "direita":
+            numOpcoes = listaEsquerda.querySelectorAll("option")
+            for(cont=0;cont<numOpcoes.length;cont++) {
+                let opcao = numOpcoes[cont]
+                if (opcao.selected) {
+                    let conteudo = listaDireita.innerHTML
+                    let valor = opcao.value
+                    let texto = opcao.text
+                    listaDireita.innerHTML = conteudo + "<option value='"+valor+"'>"+texto+"</option>"
+                    opcao.remove()
+                }
+            }
+            break;
+        case "esquerda":
+            numOpcoes = listaDireita.querySelectorAll("option")
+            for(cont=0;cont<numOpcoes.length;cont++) {
+                let opcao = numOpcoes[cont]
+                if (opcao.selected) {
+                    let conteudo = listaEsquerda.innerHTML
+                    let valor = opcao.value
+                    let texto = opcao.text
+                    listaEsquerda.innerHTML = conteudo + "<option value='"+valor+"'>"+texto+"</option>"
+                    opcao.remove()
+                }
+            }
+            break;
+        case "cima":
+            numOpcoes = listaDireita.querySelectorAll("option")
+            for(cont=0;cont<numOpcoes.length;cont++){
+                opcao = numOpcoes[cont]
+                if (opcao.selected) {
+                    if (cont>0){
+                        valor = opcao.value
+                        texto = opcao.text
+                        newPos = cont-1
+                        intermediario = document.createElement("option")
+                        intermediario = numOpcoes[cont]
+                        numOpcoes[newPos].before(intermediario)
+                    }
+                }
+            }
+            break;
+        case "baixo":
+            numOpcoes = listaDireita.querySelectorAll("option")
+            for(cont=0;cont<numOpcoes.length;cont++){
+                opcao = numOpcoes[cont]
+                if (opcao.selected) {
+                    if (cont<numOpcoes.length-1){
+                        valor = opcao.value
+                        texto = opcao.text
+                        newPos = cont+1
+                        intermediario = document.createElement("option")
+                        intermediario = numOpcoes[cont]
+                        numOpcoes[newPos].after(intermediario)
+                    }
+                }
+            }
+            break;
+        default:
+
+            break;
+    }
+
+    
+})
+
+// --------------------------------------------------------------------------
 
 // var ramais = new Chart(document.getElementById('ramais').getContext('2d'), {
 //     type: 'pie',
